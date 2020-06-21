45747f5336d710c2d9cee4eea30c949ac5305b9b
Sarah <sarah_adebabay@berkeley.edu>
2020-06-20 23:20:19 -0700

    Add map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -88,6 +88,13 @@ function addCommentToDom(comment) {
   nameContainer.innerText = comment;
 }
 
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 
 
 
